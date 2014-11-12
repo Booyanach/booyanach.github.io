258e66631a77e717eb73bfa0dcf3680f52edfccf
Gonçalo Vieira <booyanach@forklr.com>
2014-11-12 11:00:36 +0000

    now uses localStorage, made it so that it doesn't always poll github's API.

--- js/booya.js
+++ js/booya.js
@@ -4,34 +4,59 @@ $(document).ready(function() {
         
         self.loadMe();
         self.loadProjects();
+        self.drawMe();
     };
     
     module.prototype.loadProjects = function() {
-      $.get('https://api.github.com/users/booyanach/repos', function(response) {
-      });
+        var self = this;
+        
+        if(!localStorage.repos) {
+            $.get('https://api.github.com/users/booyanach/repos', function(response) {
+                localStorage.setItem('repos', JSON.stringify(response));
+                self.handleStorage('repos', response);
+            });
+        } else {
+            self.handleStorage('repos', localStorage.repos);
+        }
     };
     
     module.prototype.loadMe = function() {
-      $.get('https://api.github.com/users/booyanach', function(response) {
-          var classes = Object.keys(response);
-          $.each(classes, function(idx, item) {
-             var value = response[item];
+        var self = this;
+        
+        if (!localStorage.me) {
+         $.get('https://api.github.com/users/booyanach', function(response) {
+             localStorage.setItem('me', JSON.stringify(response));
+             self.handleStorage('me', response);
+         });
+        } else {
+            self.handleStorage('me', localStorage.me);
+        }
+    };
+    
+    module.prototype.handleStorage = function(key, value) {
+        this[key] = JSON.parse(value);
+    };
+    
+    module.prototype.drawMe = function() {
+        var self = this,
+            classes = Object.keys(this.me);
+        $.each(classes, function(idx, item) {
+            var value = self.me[item];
              
-             $('.loadMe').each(function(i, elm) {
-                 var element = $(this);
+            $('.loadMe').each(function(i, elm) {
+                var element = $(this);
                      
-                 if (element.hasClass(item)) {
-                     if (element.hasClass('text')) {
+                if (element.hasClass(item)) {
+                    if (element.hasClass('text')) {
                         element.html(value); 
-                     } else if(element.hasClass('image')) {
-                         element.css({
-                             'background-image': 'url("' + value + '")'
-                         })
-                     }
-                 }
-             })
-          });
-      });
+                    } else if(element.hasClass('image')) {
+                        element.css({
+                            'background-image': 'url("' + value + '")'
+                        })
+                    }
+                }
+            })
+        });
     };
     
     var booya = new module();
