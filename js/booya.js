bc1180ca640e92e18df9b33e1032b628f223fac4
Gonçalo Vieira <booyanach@forklr.com>
2014-11-12 15:37:49 +0000

    added a polyfill for object.watch, added watch calls to only render elements when they actually exist.

--- js/booya.js
+++ js/booya.js
@@ -7,7 +7,14 @@ $(document).ready(function() {
     };
     
     module.prototype.loadPage = function() {
-        var self = this;
+        var self = this,
+            now = new Date().getTime();
+        if (!localStorage.date || localStorage.date && localStorage.date < now) {
+            delete localStorage.repos;
+            delete localStorage.me;
+            delete localStorage.date;
+            localStorage.setItem('date', now);
+        }
         
         if(!localStorage.repos) {
             $.get('https://api.github.com/users/booyanach/repos', function(response) {
@@ -29,42 +36,94 @@ $(document).ready(function() {
     };
     
     module.prototype.handleStorage = function(key, value) {
-        this[key] = JSON.parse(value);
+        this[key] = typeof value === 'string' ? JSON.parse(value) : value;
     };
     
     module.prototype.draw = function() {
-        var self = this,
-            classes = Object.keys(self.me);
-        $.each(classes, function(idx, item) {
-            var value = self.me[item];
-             
-            $('.loadMe').each(function(i, elm) {
-                var element = $(this);
-                     
-                if (element.hasClass(item)) {
-                    if (element.hasClass('text')) {
-                        element.html(value); 
-                    } else if(element.hasClass('image')) {
-                        element.css({
-                            'background-image': 'url("' + value + '")'
-                        });
+        var self = this;
+        
+        self.watch('me', function(id, oldval, newval) {
+            var classes = Object.keys(newval);
+            $.each(classes, function(idx, item) {
+                var value = newval[item];
+                 
+                $('.loadMe').each(function(i, elm) {
+                    var element = $(this);
+                         
+                    if (element.hasClass(item)) {
+                        if (element.hasClass('text')) {
+                            element.html(value); 
+                        } else if(element.hasClass('image')) {
+                            element.css({
+                                'background-image': 'url("' + value + '")'
+                            });
+                        }
                     }
-                }
-            })
+                })
+            });
         });
         
-         $.each(self.repos, function(index, project) {
-             console.log(project);
-            var container = $('<div class="project"/>').on('click', function() {
-                var win = window.open(project.html_url, '_blank');
-                win.focus();
-            }),
-                name = $('<div class="name"/>').html(project.name).appendTo(container),
-                language = $('<div class="language"/>').html(project.language);
-            
-            container.appendTo($('.projects'));
+        self.watch('repos', function(id, oldval, newval) {
+             $.each(newval, function(index, project) {
+                 console.log(project);
+                var container = $('<div class="project"/>').on('click', function() {
+                    var win = window.open(project.html_url, '_blank');
+                    win.focus();
+                }),
+                    name = $('<div class="name"/>').html(project.name).appendTo(container),
+                    language = $('<div class="language"/>').html(project.language);
+                
+                container.appendTo($('.projects'));
+            });
         });
     };
     
     var booya = new module();
 });
+
+/******************* POLYFILL ***************************/
+// As per https://gist.github.com/eligrey/384583
+/********************************************************/
+if (!Object.prototype.watch) {
+	Object.defineProperty(Object.prototype, "watch", {
+		  enumerable: false
+		, configurable: true
+		, writable: false
+		, value: function (prop, handler) {
+			var
+			  oldval = this[prop]
+			, newval = oldval
+			, getter = function () {
+				return newval;
+			}
+			, setter = function (val) {
+				oldval = newval;
+				return newval = handler.call(this, prop, oldval, val);
+			}
+			;
+			
+			if (delete this[prop]) { // can't watch constants
+				Object.defineProperty(this, prop, {
+					  get: getter
+					, set: setter
+					, enumerable: true
+					, configurable: true
+				});
+			}
+		}
+	});
+}
+ 
+// object.unwatch
+if (!Object.prototype.unwatch) {
+	Object.defineProperty(Object.prototype, "unwatch", {
+		  enumerable: false
+		, configurable: true
+		, writable: false
+		, value: function (prop) {
+			var val = this[prop];
+			delete this[prop]; // remove accessors
+			this[prop] = val;
+		}
+	});
+}
